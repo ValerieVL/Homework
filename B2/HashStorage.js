a43987f160d2aae747a8bc051ef23d9dad6c9e56
Valery Kuchko <valery.kuchko@gmail.com>
2017-03-03 19:14:31 +0300

    Add files via upload

--- B2/HashStorage.js
+++ B2/HashStorage.js
@@ -11,7 +11,7 @@ function THashStorage() {
         if (key in hash)
             return hash[key];
         else
-            return "undefined";
+            return undefined;
     };
 
     this.deleteValue = function (key) {
@@ -23,30 +23,6 @@ function THashStorage() {
     };
 }
 
-var drinkStorage = new THashStorage();
 
 
-function addDrink() {
-    var drink = prompt("Введите название напитка:", "Маргарита");
-    var info = [confirm("Алкогольный он или нет? (ОК - да, Отмена - нет)"), prompt("Введите рецепт приготовления напитка:")];
-    info[0] == true ? info[0] = "да" : info[0] = "нет";
-    drinkStorage.addValue(drink, info);
-}
 
-function getInfoAboutDrink() {
-    var drinkAsk = prompt("Введите название напитка, информацию о котором Вы хотите узнать:", "Маргарита");
-    var result = drinkStorage.getValue(drinkAsk);
-    if (result != "undefined")
-        alert("Напиток: " + drinkAsk + "\nАлкогольный: " + result[0] + "\nРецепт приготовления: " + result[1]);
-    else
-        alert("Такого напитка нет!");
-}
-
-function deleteDrink() {
-    var delDrink = prompt("Введите название напитка, которого хотите удалить:");
-    drinkStorage.deleteValue(delDrink);
-}
-
-function getAllDrinks() {
-    alert(drinkStorage.getKeys());
-}
